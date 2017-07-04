60e4fe848025c9dab2736fa60dc88e58425b17dc
Stachuu87 <piotr.stanislawski@mail.com>
2017-07-04 13:56:28 +0200

    Added styles to team section + portfolio gallery made

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 	var resize = function () {
 		if (window.innerWidth >= 640) {
 			menuList.fadeIn();
-			hamburger.fadeOut();
+			hamburger.hide();
 		} else {
 			menuList.hide();
 			hamburger.fadeIn();
@@ -20,4 +20,46 @@ $(document).ready(function () {
 	window.addEventListener("resize", function () {
 		resize();
 	});
+	
+	var allPics = $(".pics").find("img");
+	var stylePics = $(".pics").find(".style");
+	var colourPics = $(".pics").find(".colour");
+	var cutPics = $(".pics").find(".cut");
+	var styleBtn = $(".styleBtn");
+	var colourBtn = $(".colourBtn");
+	var cutBtn = $(".cutBtn");
+	var allBtn = $(".allBtn");
+	var fullScreen = $(".fullScreen");
+	var closeBtn = $(".fullScreenClose")
+
+	styleBtn.on("click", function(){
+		allPics.hide();
+		stylePics.fadeIn();
+	})
+	cutBtn.on("click", function(){
+		allPics.hide();
+		cutPics.fadeIn();
+	})
+	colourBtn.on("click", function(){
+		allPics.hide();
+		colourPics.fadeIn();
+	})
+	allBtn.on("click", function(){
+		allPics.fadeIn();
+	})
+	
+	allPics.on("click", function(event){
+		var toAppend = $(event.target).clone();
+		fullScreen.find(".picContainer").append(toAppend);
+		fullScreen.fadeIn();
+		fullScreen.removeClass("hidden");
+	})
+	
+	closeBtn.on("click", function(){
+		var child = fullScreen.find("img");
+		child.hide();
+		fullScreen.fadeOut();
+		fullScreen.addClass("hidden");
+	})
+	
 });
